7945d45120489af3852d2fed81d9a37bccee5197
PANKAJ19-coder <pankaj1952005@gmail.com>
2026-03-19 23:01:05 +0530

    Did small error written /listings instead of /listing

--- app.js
+++ app.js
@@ -78,7 +78,7 @@ app.use((req, res, next)=>{
 
 
 app.get("/",(req, res)=>{
-    res.redirect("/listings");
+    res.redirect("/listing");
 });
 app.use("/listing", listingRouter)
 app.use("/listing/:id/review", reviewRouter);
